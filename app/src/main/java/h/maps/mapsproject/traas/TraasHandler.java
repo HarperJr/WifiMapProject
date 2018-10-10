0969a7606a5c67ce5a7ae697b2c595575a83337d
MAXIMBEREZIN <mb@digipeople.ru>
2018-10-10 15:26:54 +0300

    Добавлена TraaS library

---
+++ app/src/main/java/h/maps/mapsproject/traas/TraasHandler.java
@@ -0,0 +1,15 @@
+package h.maps.mapsproject.traas;
+
+import it.polito.appeal.traci.SumoTraciConnection;
+
+/**
+ * @author Maxim Berezin
+ */
+public class TraasHandler {
+
+    private SumoTraciConnection connection;
+
+    public void connect() {
+        //connection = new SumoTraciConnection()
+    }
+}
